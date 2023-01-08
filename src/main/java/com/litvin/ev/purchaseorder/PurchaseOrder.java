3cc2a3ed3bf84b32e5758f3c295a049a6d9c6913
Evgenii Litvin <ev_litvin@yahoo.com>
2023-01-08 20:00:04 +0500

    Modify main logic and style.

--- src/main/java/com/litvin/ev/purchaseorder/PurchaseOrder.java
+++ src/main/java/com/litvin/ev/purchaseorder/PurchaseOrder.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
@@ -14,5 +17,13 @@ public class PurchaseOrder {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    private String contractNumber;
+    private String contractAddendum;
+    private String isotope;
+    private double isotopeEnrichment;
+    private String isotopeChemicalForm;
+    private double isotopeElementaryWeight;
+    private double isotopeLigatureWeight;
 
 }
--- src/main/java/com/litvin/ev/purchaseorder/controllers/PurchaseOrdersController.java
+++ src/main/java/com/litvin/ev/purchaseorder/controllers/PurchaseOrdersController.java
@@ -1,15 +1,58 @@
 package com.litvin.ev.purchaseorder.controllers;
 
+import com.litvin.ev.purchaseorder.PurchaseOrder;
+import com.litvin.ev.purchaseorder.PurchaseOrderUser;
+import com.litvin.ev.purchaseorder.data.PurchaseOrderRepository;
+import com.litvin.ev.purchaseorder.data.PurchaseOrderUserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RequestMapping("/purchase_orders")
 @Controller
+@SessionAttributes("order")
 public class PurchaseOrdersController {
 
+    final PurchaseOrderRepository purchaseOrderRepository;
+    final PurchaseOrderUserRepository purchaseOrderUserRepository;
+
+    public PurchaseOrdersController(PurchaseOrderRepository purchaseOrderRepository,
+                                    PurchaseOrderUserRepository purchaseOrderUserRepository) {
+        this.purchaseOrderRepository = purchaseOrderRepository;
+        this.purchaseOrderUserRepository = purchaseOrderUserRepository;
+    }
+
+    @ModelAttribute(name = "purchaseOrder")
+    public PurchaseOrder purchaseOrder() {
+        return new PurchaseOrder();
+    }
+
+    @ModelAttribute(name = "purchaseOrderUser")
+    public PurchaseOrderUser purchaseOrderUser(Principal principal) {
+        String personalNumber = principal.getName();
+        return purchaseOrderUserRepository.findByPersonalNumber(personalNumber);
+    }
+
+    @ModelAttribute(name = "date")
+    public String date() {
+        String pattern = "dd MMMM yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        return simpleDateFormat.format(new Date());
+    }
+
     @GetMapping
-    public String showPurchaseOrdersPage() {
+    public String showPurchaseOrdersPage(Model model) {
+        model.addAttribute("allPurchaseOrders", purchaseOrderRepository.findAll());
         return "purchase_orders";
+    }
+
+    @PostMapping()
+    public String addPurchaseOrder(PurchaseOrder purchaseOrder) {
+        purchaseOrderRepository.save(purchaseOrder);
+        return "redirect:/purchase_orders";
     }
 }
